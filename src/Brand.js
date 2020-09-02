11743385d56646a20a907745283e1d514e2e5dcf
Juliano Costa <apfjuliano@gmail.com>
2020-09-01 22:48:49 -0300

    Configuring Bootstrap/Reactstrap

---
+++ src/Brand.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const company = {id:0, name: "Con Fusion"}
+
+function Brand() {
+    return (
+        <h4>
+            Ristorante {company.name}
+        </h4>
+    );
+  }
+  
+  export default Brand;
