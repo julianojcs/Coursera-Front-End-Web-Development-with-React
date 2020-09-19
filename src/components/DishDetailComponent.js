43982c29144efd888bd7ec4f0ce32978d43b285d
Juliano Costa <apfjuliano@gmail.com>
2020-09-19 16:39:10 -0300

    Fetch from Server (json-server)

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form'
 import Rating from 'react-rating';
 import { Loading} from './LoadingComponent';
+import { baseUrl } from '../shared/baseURL';
 
 const required = (val) => val && val.length
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -13,7 +14,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
         return(
             <div className="col-12 col-md-5 m-1 mx-auto">
                 <Card className="mx-auto">
-                    <CardImg src={dish.image} alt={dish.name} />
+                    <CardImg src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
